8440ac35e1eb0776eb769d8aa6d0956fa19f53bd
Yotsuba <happy819tw@gmail.com>
2025-06-05 16:38:11 +0800

    解決設定腳本在虛擬機執行就正常，實體機就不正常的問題
    解決辦法就是直接把 .bat 的中文註解改成英文註解就好
    
    我發現到中文註解在實體機會被當成指令執行，問 ChatGPT 說可能是因為編碼問題導致
    
    總之改成英文註解就好了

--- mpv-yotsuba-setup.bat
+++ mpv-yotsuba-setup.bat
@@ -1,19 +1,19 @@
 @echo off
 
-:: 切換到 .bat 所在的目錄（解決管理員模式下工作目錄錯誤問題）
+:: Change to the directory where the batch is
 cd /d "%~dp0"
 
-:: 取得當前目錄
+:: Get current directory
 set "CUR_DIR=%CD%"
 
-:: 加入跳脫字元
+:: Add escape characters
 set "CUR_DIR=%CUR_DIR:\=\\%"
 
-:: 準備最終要寫入的路徑
+:: Prepare mpv path
 set "MPV_PATH=%CUR_DIR%\\mpv.exe"
 
-:: 寫入設定檔
+:: Write config file
 echo mpv = "%MPV_PATH%" > "mpv-handler\config.toml"
 
-:: 執行 mpv-handler/handler-install.bat
+:: Run handler install script
 call "mpv-handler\handler-install.bat"
